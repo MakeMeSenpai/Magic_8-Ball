55a0019a59f23575f0c98715305f36cfab26cb0d
Anthony <sean.a.protho1@gmail.com>
2020-11-25 19:42:10 -0800

    Initial Commit

---
+++ Magic_8-Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic_8-Ball
+//
+//  Created by Anthony Protho on 11/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
